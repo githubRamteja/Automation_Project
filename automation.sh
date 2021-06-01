5922ce8f3ee6e58a8ad80278f717c900f4d80e3b
SriRamTeja Paladi <paladitj@gmail.com>
2021-06-01 19:58:56 +0000

    Committing the task 3 changes

--- automation.sh
+++ automation.sh
@@ -12,7 +12,19 @@ s3_bucket="upgrad-sriramteja"
 status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
 if [ ! $? = 0 ] || [ ! "$status" = installed ];
 then
+	echo "Installing apache2 package"
 	sudo apt install $pkg -y
+fi
+
+#Checking if apache2 service is enabled or not
+apache_service_status=$(systemctl is-enabled apache2)
+if [[ $apache_service_status == "enabled" ]];
+then
+	echo "Apache2 service is enabled."
+else
+	echo "Enabling apache2 service"
+	sudo systemctl enable apache2
+	echo "Apache2 service is now enabled."
 fi
 
 #Checking if apache2 service is running and starting it if not already.
@@ -22,6 +34,7 @@ if [[ $apache_status == *"active (running)"* ]];
 then
 	echo "Apache service is running"
 else
+	echo "Starting apache2 service"
 	sudo systemctl start apache2
 fi
 
@@ -36,5 +49,33 @@ aws s3 \
 cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
 s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
 
-#Confirmation message after copying to the S3 Bucket
 echo "Copied to S3 Bucket $s3_bucket"
+
+#calculating tar file size
+tar_size="$(du -h "${myname}-httpd-logs-${timestamp}.tar" | cut -f1)"
+
+html_record="<p>httpd-logs\&emsp;\&emsp;\&emsp;$timestamp\&emsp;\&emsp;\&emsp;tar\&emsp;\&emsp;\&emsp;$tar_size<\/p>"
+html_file="/var/www/html/inventory.html"
+
+
+if [[ ! -f /var/www/html/inventory.html ]]
+then
+        touch /var/www/html/inventory.html
+	sudo chmod 666 /var/www/html/inventory.html
+        echo -e "<html>\n<body>\n<h3>Log Type&emsp;&emsp;Time Created&emsp;&emsp;Type&emsp;&emsp;Size</h3>\n</body>\n</html>" >> /var/www/html/inventory.html
+else
+        echo "The inventory.html file exists"
+	sudo sed -i "/<\/body>/i $html_record" "$html_file"
+	echo "Appended the log data"
+fi
+
+
+if [[ ! -f /etc/cron.d/automation ]]
+then
+        sudo touch /etc/cron.d/automation
+        echo "0 0 * * * root /root/Automation_Project/automation.sh" |sudo tee /etc/cron.d/automation
+        sudo chmod +x /etc/cron.d/automation
+	echo "Automation cron job is created in /etc/cron.d/"
+else
+        echo "The automation cron job exists"
+fi
